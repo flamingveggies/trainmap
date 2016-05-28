c28d9c8d72d16087b28b041d088a0133183d501d
Merritt Lawrenson <merrittlawrenson@gmail.com>
2016-05-28 20:22:20 +0000

    an attempt to start objectifying things

--- js/app2.js
+++ js/app2.js
@@ -11,15 +11,43 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     accessToken: 'pk.eyJ1IjoiZmxhbWluZ3ZlZ2dpZXMiLCJhIjoiY2lodGd4dDJzMDE5ZXUxbTF5czU1a3BxeCJ9.iqB50rVPS3yINubr2h1mbQ'
 }).addTo(map);
 
-var trainMarkers = new L.LayerGroup().addTo(map);
-var busMarkers = new L.LayerGroup().addTo(map);
+L.control.locate().addTo(map);
 
-var overlays = {
-  "Trains": trainMarkers,
-  "Busses": busMarkers
-};
+var trainMarkers, busMarkers, overlays;
+var routeList = [];
 
-L.control.layers(null, overlays).addTo(map);
+function initialize() {
+  trainMarkers = new L.LayerGroup().addTo(map);
+  busMarkers = new L.LayerGroup().addTo(map);
+  
+  overlays = {
+    "Trains": trainMarkers,
+    "Busses": busMarkers
+  };
+  
+  L.control.layers(null, overlays).addTo(map);
+  
+  $.getJSON("https://developer.trimet.org/ws/v2/vehicles?appID=D065A3A5DAE4622752786CEB9", function(data) {
+    $.each(data.resultSet.vehicle, function(vehicle) {
+      routeList.push(this.routeNumber);
+    });
+    routeList = uniq(routeList);
+    routeList = routeList.sort(function (a, b) {  return a - b;  });
+    console.log(routeList);
+  });
+  
+  $.each(routeList, function(route) {
+    overlays.push({ route : new L.LayerGroup().addTo(map) })
+  })
+}
+
+// remove duplicate numbers
+function uniq(a) {
+  var seen = {};
+  return a.filter(function(item) {
+      return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+  });
+}
 
 function parseDelay(seconds) {
   var absSeconds = Math.abs(seconds);
@@ -36,7 +64,12 @@ function getVehicles(url) {
   $.getJSON(url, function(data) {
     trainMarkers.clearLayers();
     busMarkers.clearLayers();
-    $.each(data.resultSet.vehicle, function(i, vehicle) {
+    parseVehicles(data);
+  });
+}
+
+function parseVehicles(data) {
+  $.each(data.resultSet.vehicle, function(i, vehicle) {
       var vehicleRoute;
       var delayMessage;
       if (vehicle.routeNumber === 90) {
@@ -75,7 +108,6 @@ function getVehicles(url) {
       // .addTo(trainMarkers)
       // .bindPopup("<b>" + vehicle.signMessageLong + "</b><br>" + delayMessage);
     });
-  });
 }
 
 function refreshVehicles() {
@@ -94,5 +126,6 @@ function refreshVehicles() {
 }
 
 
+initialize();
 refreshVehicles();
 setInterval(refreshVehicles, 30000);
